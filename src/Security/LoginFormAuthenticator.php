068889d119ff00a5b61afb97671091245b708b00
Yannick <yannkhe@gmail.com>
2023-12-02 14:26:33 +0100

    handle remenber me

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -13,6 +13,8 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
@@ -38,16 +40,23 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         $password = (string) $request->request->get('password');
         
         return new Passport(
-          new UserBadge($email, function ($userIdentifier) {
-              $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+              new UserBadge($email, function ($userIdentifier) {
+                  $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
 
-              if (!$user) {
-                  throw new UserNotFoundException();
-              }
+                  if (!$user) {
+                      throw new UserNotFoundException();
+                  }
 
-              return $user;
-          }),
-          new PasswordCredentials($password)
+                  return $user;
+              }),
+              new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge(
+                    'authenticate',
+                    (string) $request->request->get('_csrf_token')
+                ),
+                (new RememberMeBadge())->enable(),
+            ]
         );
     }
 
